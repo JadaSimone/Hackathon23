9d620dd753600f8d367b2b130d99d743146b0aee
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-15 17:09:03 -0500

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+state_measure=$(jq .state_measure api_input.json | tr -d "\"")
+s3_state_upload=$(jq .state_store.location api_input.json | tr -d "\"")
+
+# measure state
+curl --url $state_measure > state_measure.py
+echo $(python state_measure.py) > state.txt
+
+# upload state
+aws s3 cp state.txt $s3_state_upload
+
+# stop process
+# shutdown -h now
