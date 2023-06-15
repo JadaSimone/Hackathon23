2bf8278d838964492bbf5b8b2999bedb8c9bea87
sgward3 <136746119+sgward3@users.noreply.github.com>
2023-06-15 15:28:19 -0500

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,14 +1,14 @@
 import time
 
-def count_values (maxVal, sleepTimer):
-    for i in range(maxVal):
+def count_values (startVal=0, maxVal=10, sleepTimer=1):
+    for i in range(startVal, maxVal):
         print(i)
         file = open("../spot_state.txt", "w")
         file.write(str(i))
         file.close()
         time.sleep(sleepTimer)
 
-count_values(10, 5)
+count_values(3, 10, 1)
 file = open("../spot_state.txt", "r")
 print()
 print(file.read())
