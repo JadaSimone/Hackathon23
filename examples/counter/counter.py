50ad67a7b2ee955969aeeda62c205a3b9ac8a839
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-15 22:00:40 -0500

    Update counter.py

--- examples/counter/counter.py
+++ examples/counter/counter.py
@@ -1,12 +1,11 @@
 import time
 import sys
+import state_save
 
 def count_values(startVal=0, maxVal=10, sleepTimer=1):
     for i in range(startVal, maxVal):
         print(i)
-        file = open("../spot_state.txt", "w")
-        file.write(str(i))
-        file.close()
+        state_save.save(i)
         time.sleep(sleepTimer)
 
 if __name__ == '__main__':
