4599e5f85b6ccb0f32a62ca0b430fe72b1f0f4cb
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-15 22:27:57 -0500

    Update stop_compute_lambda.py

--- lambdas/stop_compute_lambda.py
+++ lambdas/stop_compute_lambda.py
@@ -1,8 +1,10 @@
-import json
 import boto3
-    
+COMMAND = 'touch /opt/hackathon/test.sh'
+#COMMAND = '/opt/hackathon/stop.sh'
+
 def lambda_handler(event, context):
     ssm_client = boto3.client('ssm', region_name='us-east-1')
-    print('client made {}'.format(ssm_client))
-    instance_id = 'i-01762e272a68629a4'
-    response = ssm_client.send_command( InstanceIds=[instance_id], DocumentName="AWS-RunShellScript", Parameters={'commands': ['touch /opt/hackathon/test.sh']})
+    instance_id = 'i-01762e272a68629a4' # TODO: get this from the terraform file
+    
+    print('running command {} on ec2 instance {}'.format(COMMAND, instance_id))
+    response = ssm_client.send_command( InstanceIds=[instance_id], DocumentName='AWS-RunShellScript', Parameters={'commands': [COMMAND]})
