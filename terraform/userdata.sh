7467659e4b784eb59230873e13a1c75380678558
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-16 11:18:40 -0500

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -5,10 +5,12 @@ pip3 install boto3
 mkdir /opt/hackathon/
 cd /opt/hackathon/
 aws s3 cp ${s3_bucket_path}
-echo "REQUEST_ID=${var.request_id}" | sudo tee -a /opt/hackathon/env.sh
             
 . /opt/hackathon/env.sh
 svn export https://github.com/JadaSimone/Hackathon23/trunk/ec2_scripts
 chmod +x /opt/hackathon/ec2_scripts/start.sh
 chmod +x /opt/hackathon/ec2_scripts/stop.sh
+input_file=https://raw.githubusercontent.com/JadaSimone/Hackathon23/main/examples/counter/input.json # TODO : pass this in from terraform. don't hard code
+curl --url $input_file > input.json
+
 /opt/hackathon/ec2_scripts/start.sh
