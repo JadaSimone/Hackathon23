6fc731018d45abcf65d71db45243808c2d28034e
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-16 12:09:45 -0500

    Update stop_compute_lambda.py

--- lambdas/stop_compute_lambda.py
+++ lambdas/stop_compute_lambda.py
@@ -1,10 +1,11 @@
 import boto3
-COMMAND = 'touch /opt/hackathon/test.sh'
-#COMMAND = '/opt/hackathon/stop.sh'
+COMMAND = '/opt/hackathon/stop.sh'
 
 def lambda_handler(event, context):
     ssm_client = boto3.client('ssm', region_name='us-east-1')
-    instance_id = 'i-01762e272a68629a4' # TODO: get this from event!
+    print(event)
+    
+    instance_id = event['detail']['instance-id']
     
     print('running command {} on ec2 instance {}'.format(COMMAND, instance_id))
     response = ssm_client.send_command( InstanceIds=[instance_id], DocumentName='AWS-RunShellScript', Parameters={'commands': [COMMAND]})
