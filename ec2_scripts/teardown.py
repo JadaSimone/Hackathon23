e455b1a3308972c6cf58dc212128ea2c27822dba
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-16 08:54:25 -0500

    Update teardown.py

--- ec2_scripts/teardown.py
+++ ec2_scripts/teardown.py
@@ -1,14 +1,16 @@
-# trigger lambda to run terraform destroy 
+# trigger lambda to run terraform destroy
 import boto3
 import subprocess
+import json
 import os
-requestId = os.environ.get('REQUEST_ID')
+requestId = os.environ.get('REQUEST_ID','')
+print("tearing down requestId: {}".format(requestId))
 
 # topic name should be specific per input & only terminate that one deployment
-lambda_client = boto3.client('lambda')
+lambda_client = boto3.client('lambda',region_name='us-east-1')
 lambda_payload = {'requestID':requestId}
-lambda_client.invoke(FunctionName='terraform_destroy_lambda', 
+lambda_client.invoke(FunctionName='terraform_destroy_lambda',
                      InvocationType='Event',
-                     Payload=lambda_payload)
+                     Payload=json.dumps(lambda_payload))
 
 subprocess.run(['/opt/hackathon/ec2_scripts/stop.sh'],shell=True)
