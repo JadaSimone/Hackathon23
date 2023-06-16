f79df4aa00da6c50f796d788c9fd300805f2f9db
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-16 11:27:25 -0500

    Update start.sh

--- ec2_scripts/start.sh
+++ ec2_scripts/start.sh
@@ -3,6 +3,7 @@
 compute=$(jq .compute input.json | tr -d "\"")
 requirements=$(jq .requirements[] input.json | tr -d "\"")
 s3_state=$(jq .state_store.location input.json | tr -d "\"")
+state_save=$(jq .state_save input.json | tr -d "\"")
 
 # install the requirements
 for requirement in $requirements
@@ -12,6 +13,7 @@ done
 
 # install what we need to compute
 curl --url $compute > compute.py
+curl --url $state_save > state_save.py
 
 # check the current state
 aws s3 cp $s3_state state.txt
