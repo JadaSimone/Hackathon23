b06f49cb4296e06a9384903fbc321627249fcf59
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-16 07:54:58 -0500

    Update teardown.py

--- ec2_scripts/teardown.py
+++ ec2_scripts/teardown.py
@@ -1,6 +1,14 @@
-# sends a messages to SNS to run terraform destroy 
+# trigger lambda to run terraform destroy 
 import boto3
+import subprocess
+import os
+requestId = os.environ.get('REQUEST_ID')
 
-client = boto3.get_client('sns')
 # topic name should be specific per input & only terminate that one deployment
-client.publish(TopicArn='TBD', Message=f'tear down infrastructure')
+lambda_client = boto3.client('lambda')
+lambda_payload = {'requestID':requestId}
+lambda_client.invoke(FunctionName='terraform_destroy_lambda', 
+                     InvocationType='Event',
+                     Payload=lambda_payload)
+
+subprocess.run(['/opt/hackathon/ec2_scripts/stop.sh'],shell=True)
