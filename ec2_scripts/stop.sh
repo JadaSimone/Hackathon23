9573a08abc9e5c54bd87c8c25aa687f591511034
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-16 08:21:41 -0500

    Update stop.sh

--- ec2_scripts/stop.sh
+++ ec2_scripts/stop.sh
@@ -5,7 +5,7 @@ s3_state_upload=$(jq .state_store.location api_input.json | tr -d "\"")
 
 # measure state
 curl --url $state_measure > state_measure.py
-echo $(python state_measure.py) > state.txt
+echo $(python3 state_measure.py) > state.txt
 
 # upload state
 aws s3 cp state.txt $s3_state_upload
