c88912ee70c4b11d45f5a8932e531c32d94f2284
Tyler Wooten <tylerwooten99@gmail.com>
2023-06-15 21:16:55 -0500

    Update terraform_apply_lambda.py

--- lambdas/terraform_apply_lambda.py
+++ lambdas/terraform_apply_lambda.py
@@ -17,14 +17,20 @@ TERRAFORM_DOWNLOAD_URL = (
 TERRAFORM_DIR = os.path.join('/tmp', 'terraform_%s' % TERRAFORM_VERSION)
 TERRAFORM_PATH = os.path.join(TERRAFORM_DIR, 'terraform')
 
-GITHUB_REPO = f"https://github.com/JadaSimone/Hackathon23.git"
-CLONE_LOCATION = '/tmp/xmarksspot'
+GIT_REPO = "https://github.com/JadaSimone/Hackathon23.git"
+CLONE_TO_DIR = "/tmp/xmarksspot"
 
 def lambda_handler(event, context):
-    clone(GITHUB_REPO)
-    install_terraform()
-    apply_terraform_plan()
+    for record in event['Records']: # loop so we can have a multiple kick off in one upload
+        bucket = record['s3']['bucket']['name']
+        obj_name = record['s3']['object']['key']
+        s3_input = "s3://{}/{}".format(bucket, obj_name)
+        print("new input: {}".format(s3_input))
     
+        #clone(GIT_REPO)
+        #install_terraform(TERRAFORM_PATH)
+        #apply_terraform_plan()
+        
 def check_call(args):
     """Wrapper for subprocess that checks if a process runs correctly,
     and if not, prints stdout and stderr.
@@ -41,16 +47,16 @@ def check_call(args):
             returncode=proc.returncode,
             cmd=args)
 
-def install_terraform():
+def install_terraform(path):
     """Install Terraform on the Lambda instance."""
     # Most of a Lambda's disk is read-only, but some transient storage is
     # provided in /tmp, so we install Terraform here.  This storage may
     # persist between invocations, so we skip downloading a new version if
     # it already exists.
     # http://docs.aws.amazon.com/lambda/latest/dg/lambda-introduction.html
-    print(TERRAFORM_PATH)
+    print(path)
     
-    if os.path.exists(TERRAFORM_PATH):
+    if os.path.exists(path):
         return
     
     urllib.request.urlretrieve(TERRAFORM_DOWNLOAD_URL, '/tmp/terraform.zip')
@@ -59,27 +65,25 @@ def install_terraform():
     #   '-o' = overwrite existing files without prompting
     #   '-d' = output directory
     check_call(['unzip', '-o', '/tmp/terraform.zip', '-d', TERRAFORM_DIR])
-    check_call([TERRAFORM_PATH, '--version'])
+
+    check_call([path, '--version'])
 
 def apply_terraform_plan():
-    os.chdir(CLONE_LOCATION)
-    #os.system('terraform plan'))
+    os.chdir(CLONE_TO_DIR + '/terraform')
     check_call([TERRAFORM_PATH, '--version'])
     check_call([TERRAFORM_PATH, 'init','-input=false'])
-    check_call([TERRAFORM_PATH, 'apply', CLONE_LOCATION])
+    check_call([TERRAFORM_PATH, 'apply', '/tmp/xmarksspot'])
     # check_call(['terraform', 'apply', '--approve'])
 
-def clone(repoUrl):
+def clone(repoName):
     print('empty the directory')
-    files = glob.glob(CLONE_LOCATION)
+    files = glob.glob(CLONE_TO_DIR)
     for f in files:
         os.remove(f)
     try:
-        shutil.rmtree(localDir)
+        shutil.rmtree(CLONE_TO_DIR)
     except:
         pass
-    print('cloning repo')
-    check_call(['git', 'clone', repoUrl, CLONE_LOCATION])
-    #localRepo = Repo.clone_from(repoUrl, CLONE_LOCATION)
-    print(f'Cloned repo: {repoName}')
     
+    print(f'Cloning repo: {repoName}')
+    check_call(['git', 'clone', repoName, CLONE_TO_DIR])
